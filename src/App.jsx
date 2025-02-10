82b50bd20912849d32bc5d8464685ce831fae4bc
donishabduvaliyev <donishkratos413677@gmail.com>
2025-02-10 15:16:34 +0500

    changedNavigation

--- src/App.jsx
+++ src/App.jsx
@@ -91,7 +91,7 @@ function App() {
     tg.MainButton.hide(); // Hide by default
 
     const handleMainButtonClick = () => {
-      navigate("varoq");
+      navigate("adressInfo");
     };
 
     tg.MainButton.onClick(handleMainButtonClick);
