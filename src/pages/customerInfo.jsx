4d4bd8d252fe073a530f19407a1757ac90a877c0
donishabduvaliyev <donishkratos413677@gmail.com>
2025-02-20 21:18:14 +0500

    handle-main-buton-fixed-4

--- src/pages/customerInfo.jsx
+++ src/pages/customerInfo.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback, useMemo } from "react";
 import { useCart } from "../components/context";
 import YandexMapModal from "../components/yandexMaps";
 
